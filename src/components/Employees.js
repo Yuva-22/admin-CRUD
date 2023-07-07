6a3eac080796e645384a11cc691e20c2c2c5b49e
Yuvashree <yuvashree.m22122003@gmail.com>
2023-07-07 14:26:08 +0530

    Crud Operations were included

---
+++ src/components/Employees.js
@@ -0,0 +1,29 @@
+const Employees =[
+   {
+    id:"1",
+    Name:"AAAAAA",
+    Age:"12"
+   },
+   {
+    id:"2",
+    Name:"BBBBBB",
+    Age:"23"
+   },
+   {
+    id:"3",
+    Name:"CCCCCC",
+    Age:"34"
+   },
+   {
+    id:"4",
+    Name:"DDDDDD",
+    Age:"45"
+   },
+   {
+    id:"5",
+    Name:"EEEEEE",
+    Age:"56"
+   },
+];
+
+export default Employees ;
