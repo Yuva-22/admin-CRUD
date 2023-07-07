afa5c64b0eebb3c4dcc27dbd532143ec3089fa84
Yuvashree <yuvashree.m22122003@gmail.com>
2023-07-07 16:50:05 +0530

    App.js was modified

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <h1>CRUD OPERATIONS</h1>
       <Router>
           <Routes>
              <Route path="/" element={<Home />} />
